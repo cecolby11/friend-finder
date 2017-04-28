d6ac421c4868f9c56cf8aa096b8718950cfdd138
cecolby11 <cecolby11@users.noreply.github.com>
2017-04-27 20:42:21 -0500

    read/write friends data to json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 module.exports = function(app) {
   // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
@@ -12,9 +13,22 @@ module.exports = function(app) {
 
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post('/api/friends', function(request, response) {
-    // TODO
     var newFriend = request.body;
-    console.log(newFriend);
-    response.json(newFriend);
+    // TODO store new friend in friend storage
+    fs.readFile('./app/data/friends.json', function(error, data) {
+      if(error) {
+        console.log(error);
+      } else {
+        var arr = JSON.parse(data).data;
+        arr.push(newFriend);
+        var json = {"data": arr};
+        fs.writeFile('./app/data/friends.json', JSON.stringify(json), function(error) {
+          if (error) {
+            console.log(error);
+          } 
+        });
+      }
+    });
+    // TODO do some work & show new friend result
   });
 }
