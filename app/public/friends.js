95aca00e9fb9493ea747470eeb029fb26b915cae
cecolby11 <cecolby11@users.noreply.github.com>
2017-04-27 23:40:08 -0500

    javascript

---
+++ app/public/friends.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    function readJSON() {
+        // creates URL as per giphy API documenation
+        var queryURL = 'http://localhost:8080/api/friends';
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(result) {
+            console.log(result);
+            var i = result.length - 1;
+            var self = [];
+            var match = [];
+            self.push(result[i].q1.charAt(0));
+            self.push(result[i].q2.charAt(0));
+            self.push(result[i].q3.charAt(0));
+            self.push(result[i].q4.charAt(0));
+            self.push(result[i].q5.charAt(0));
+            console.log('Self array: ' + self);
+            
+            for (var i = 0; i < (result.length - 1); i++) {
+                var array = [];
+                var score = 0;
+                array.push(result[i].q1.charAt(0));
+                array.push(result[i].q2.charAt(0));
+                array.push(result[i].q3.charAt(0));
+                array.push(result[i].q4.charAt(0));
+                array.push(result[i].q5.charAt(0));
+                console.log('Other array:  ' + array + ' at ' + i);
+                for (var x = 0; x < self.length; x++) {
+                    var difference = parseInt(self[x]) - parseInt(array[x]);
+                    score += Math.abs(difference);
+                    console.log('Score in loop ' + score + ' at ' + x);
+                    
+                }
+                match.push(score);
+            }
+            console.log(match);
+            var best = Math.min.apply(null, match);
+            console.log('lowest value ' + match.indexOf(best));
+            var finalArray = [];
+            for (var y = 0; y < match.length; y++) {
+                if (match[y] === best) {
+                    finalArray.push(result[y]);
+                }
+            }
+            console.log(finalArray);
+            displayModal(finalArray);    
+        });
+    };
+    
+    readJSON();
+    console.log('its working');
+    function displayModal() {
+        $('#pop-up').modal('show');
+    }
+ });
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,20 +1,15 @@
 var express = require('express');
+var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
-
 module.exports = function(app) {
-  //A GET Route to /survey which should display the survey page.
-  app.get('/survey', function(request, response) {
-    response.sendFile(path.join(__dirname, '../public/survey.html'));
-  });
-
-  // A default USE route that leads to home.html which displays the home page.
-  app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, '../public/home.html'));
-  });
-
-  app.get('/survey/friends.js', function(request, response) {
-    console.log('HERE');
-    response.sendFile(path.join(__dirname, 'friends.js'));
-  });
-}
+    console.log('Got HTML routes');
+    app.get("/", function(request, response) {
+        response.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+    app.get("/survey", function(request, response) {
+        response.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+    app.get("/friends.js", function(request,response){
+        response.sendFile(path.join(__dirname, '../public/friends.js'));
+    });
+};
